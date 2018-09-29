0a9cfd837b796f43179fed7ed1b90f2d7c8b95cb
SiddhartLapsiwala <37965720+SiddhartLapsiwala@users.noreply.github.com>
2018-09-29 18:32:34 -0400

    Add_Mocking
    Updated code with mocking

--- API_Connect.py
+++ API_Connect.py
@@ -1,11 +1,10 @@
-import unittest
+import unittest.mock
 import requests
 
 
-def retrive_repo_details(repo_name):
-    result = dict()
+def retrive_list_of_repo(user_name):
     list_of_repo = list()
-    url = "https://api.github.com/users/" + repo_name + "/repos"
+    url = "https://api.github.com/users/" + user_name + "/repos"
     resp = requests.get(url)
     if resp.status_code != 200:
         # This means something went wrong.
@@ -16,30 +15,35 @@ def retrive_repo_details(repo_name):
             temp = data[i]
             list_of_repo.append(temp["name"])
 
-    # Get Number of Commits to each repo
-    for j in range(0, len(list_of_repo)):
-        url_commit = "https://api.github.com/repos/" + repo_name + "/"+list_of_repo[j]+"/commits"
-        resp_commit = requests.get(url_commit)
-        if resp.status_code != 200:
-            # This means something went wrong.
-            raise ValueError("Please check repository name!!")
-        else:
-            data_commit = resp_commit.json()
-            result[list_of_repo[j]] = len(data_commit)
+    return list_of_repo
+
+
+def retrive_commit(user_name, repo_name):
+    """Get Number of Commits for repo_name"""
+    result = dict()
+    url_commit = "https://api.github.com/repos/" + user_name + "/"+repo_name+"/commits"
+    resp_commit = requests.get(url_commit)
+    if resp_commit.status_code != 200:
+        # This means something went wrong.
+        raise ValueError("Please check repository name!!")
+    else:
+        data_commit = resp_commit.json()
+        result[repo_name] = len(data_commit)
 
     return result
 
 
 def main():
 
-    repo_name = input("Enter GitHub Repository Name: ")
+    user_name = input("Enter GitHub Repository Name: ")
     try:
-        result = retrive_repo_details(repo_name)
+        result = retrive_list_of_repo(user_name)
         print("Result :: \n")
         if len(result) == 0:
             print("No Repository Found.")
-        for key, value in result.items():
-            print("Repo Name: " + key + " || " + "No. of Commits: " + str(value))
+        for i in range(0, len(result)):
+            commits = retrive_commit(user_name, result[i])
+            print("Repo Name: " + result[i] + " || " + "No. of Commits: " + commits)
     except ValueError as e:
         print(str(e))
     except Exception:
@@ -49,3 +53,4 @@ def main():
 if __name__ == '__main__':
     main()
     unittest.main(exit=False, verbosity=2)
+
--- ApiTest.py
+++ ApiTest.py
@@ -1,21 +1,80 @@
-import unittest
-from API_Connect import retrive_repo_details
+import unittest.mock
+import mock
+from Assignment5.API_Connect import retrive_commit
+from Assignment5.API_Connect import retrive_list_of_repo
+
 
 class ApiTest(unittest.TestCase):
-    def test_valid_repo(self):
+    def _mock_response(
+            self,
+            status=200,
+            content="CONTENT",
+            json_data=None,
+            raise_for_status=None):
+        """
+        since we typically test a bunch of different
+        requests calls for a service, we are going to do
+        a lot of mock responses, so its usually a good idea
+        to have a helper function that builds these things
+
+        """
+        mock_resp = mock.Mock()
+        # mock raise_for_status call w/optional error
+        mock_resp.raise_for_status = mock.Mock()
+        if raise_for_status:
+            mock_resp.raise_for_status.side_effect = raise_for_status
+        # set status code and content
+        mock_resp.status_code = status
+        mock_resp.content = content
+        # add json data if provided
+        if json_data:
+            mock_resp.json = mock.Mock(
+                return_value=json_data
+            )
+        return mock_resp
+
+    @mock.patch('requests.get')
+    def test_valid_repo(self,mock_get):
         """ test for valid repo """
-        result = retrive_repo_details("SiddhartLapsiwala")
+        json_response = [{
+          'name': 'Software_Testing_567',
+        },{
+          'name': 'Stevens---810',
+        }, {
+          'name': 'Software_Testing_Assign2',
+        }, {
+          'name': 'Software_testing_HW04',
+        }, {
+          'name': 'SSW-555-GEDCOM-Project',
+        }]
+        mock_resp = self._mock_response(json_data=json_response)
+        mock_get.return_value = mock_resp
+        result = retrive_list_of_repo("SiddhartLapsiwala")
         self.assertEqual(len(result), 5)
 
-    def test_valid_commit(self):
+    @mock.patch('requests.get')
+    def test_valid_commit(self, mock_get):
         """ test for valid commit of repo """
-        result = retrive_repo_details("SiddhartLapsiwala")
-        self.assertEqual(result.get("Software_Testing_Assign2"), 10)
+        json_response = [{
+            'sha': '0e2da5e190f0ec5eb8a86227894215e11b30eb65',
+        }, {
+          'sha': '0e2da5e190f0ec5eb8a86227894215e11b30eb65',
+        }, {
+          'sha': '0e2da5e190f0ec5eb8a86227894215e11b30eb65',
+        }]
 
-    def test_invalid_repo_name(self):
+        mock_resp = self._mock_response(json_data=json_response)
+        mock_get.return_value = mock_resp
+        result = retrive_commit("SiddhartLapsiwala", "Stevens---810")
+        self.assertEqual(result["Stevens---810"], 3)
+
+    @mock.patch('requests.get')
+    def test_invalid_repo_name(self, mock_get):
         """ test for invalid repo name"""
+        mock_resp = self._mock_response(status=404)
+        mock_get.return_value = mock_resp
         with self.assertRaises(ValueError) as context:
-            retrive_repo_details("Siddhartlapsifdgdfgd")
+            retrive_list_of_repo("Siddhartlapsifdgdfgd")
 
         self.assertTrue('Please check repository name!!' in str(context.exception))
 
